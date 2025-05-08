b16848e8ef6be19cff8f93160efd12c0c56d776b
RedjanVisitacion <visitacionredjan7@gmail.com>
2025-05-08 09:46:37 +0500

    Updated

--- src/results.php
+++ src/results.php
@@ -43,6 +43,7 @@ if (!$result) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="icon" href="../img/icon.png"/>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Election Results</title>
