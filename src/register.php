bbf5517be22cb3165953c2e7270f9e0f02c63ee8
RedjanVisitacion <visitacionredjan7@gmail.com>
2025-05-26 09:39:59 +0500

    Updated

--- src/register.php
+++ src/register.php
@@ -77,6 +77,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Registration</title>
+    <link rel="icon" href="../img/icon.png"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
 
